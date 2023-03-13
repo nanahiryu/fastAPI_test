07e346098fd63fb92e1543579bf438a514d92799
nanahiryu <ryu2751x7@gmail.com>
2023-03-13 21:55:30 +0900

    first commit

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+@app.get("/fib")
+async def fib(n: int=None):
+    print(n)
+    print(f"n is int: {isinstance(n, int)}")
+    validate(n)
+    ans: int = calc_fib(n)
+    return {"result": ans}
+
+def validate(n: int):
+    if n is None:
+        raise HTTPException(status_code=400, detail="クエリパラメータnが必要です")
+    if n < 1:
+        raise HTTPException(status_code=400, detail="nは1以上である必要があります")
+
+def calc_fib(n: int) -> int:
+    if n == 1 or n == 2:
+        return 1
+    return calc_fib(n - 1) + calc_fib(n - 2)
---
+++ test/test_main.py
@@ -0,0 +1,40 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_get_fib_NR001():
+    response = client.get("/fib?n=1")
+    assert response.status_code == 200
+    assert response.json() == {"result": 1}
+
+def test_get_fib_NR002():
+    response = client.get("/fib?n=2")
+    assert response.status_code == 200
+    assert response.json() == {"result": 1}
+
+def test_get_fib_NR003():
+    response = client.get("/fib?n=10")
+    assert response.status_code == 200
+    assert response.json() == {"result": 55}
+
+def test_get_fib_ABR001():
+    response = client.get("/fib")
+    assert response.status_code == 400
+    assert response.json() == {"detail": "クエリパラメータnが必要です"}
+
+def test_get_fib_ABR002():
+    response = client.get("/fib?n=abc")
+    assert response.status_code == 422
+    assert response.json() == {"detail": [{"loc":["query","n"],"msg":"value is not a valid integer","type":"type_error.integer"}]}
+
+def test_get_fib_ABR003():
+    response = client.get("/fib?n=0")
+    assert response.status_code == 400
+    assert response.json() == {"detail": "nは1以上である必要があります"}
+
+def test_get_fib_ABR004():
+    response = client.post("/fib?n=1")
+    assert response.status_code == 405
+    assert response.json() == {"detail": "Method Not Allowed"}
+
